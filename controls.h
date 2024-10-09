280fa30bb0c1b0f3f333bd000e7ffc32d347d025
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-10-08 22:43:52 -0400

    Declared `Label` and `Button` controls

--- controls.h
+++ controls.h
@@ -15,8 +15,6 @@ namespace btui {
         // └────────────┴────────────┘
 
         class Canvas : public Control {
-            std::mutex mtx;
-
             BufferGrid buffer;
             Align horizontalAlign;
             Align verticalAlign;
@@ -24,6 +22,8 @@ namespace btui {
             SizeU32 lastPartSize;
 
         protected:
+            std::mutex mtx;
+
             virtual void DrawControl(BufferGrid Buffer, RectU32 Partition) override;
 
         public:
@@ -45,38 +45,69 @@ namespace btui {
             virtual backgroundFill_t GetBackgroundFill();
             virtual void SetBackgroundFill(backgroundFill_t NewFill);
         };
-        //class Label : Control {
-        //    std::mutex mtx;
-        //
-        //    std::wstring text;
-        //    uint32_t textBackcolor;
-        //    uint32_t textForecolor;
-        //    Align textHorizontalAlign;
-        //    Align textVerticalAlign;
-        //    backgroundFill_t surrounding;
-        //};
-        //class Button : Control {
-        //    std::mutex mtx;
-        //
-        //    std::wstring text;
-        //    uint32_t textBackcolor;
-        //    uint32_t textForecolor;
-        //    Align textHorizontalAlign;
-        //    Align textVerticalAlign;
-        //    backgroundFill_t surroundingSelected;
-        //    backgroundFill_t surroundingUnselected;
-        //};
-        //class Table : Control {
-        //    std::mutex mtx;
-        //
-        //    uint32_t columnCount;
-        //    uint32_t rowCount;
-        // 
-        //    // Other stuff, maybe? Merged cells?
-        //    // Pointers to child controls? (NOT
-        //    // UNIQUE POINTERS!!!, just Control*-
-        //    // pointers. :). Something else will
-        //    // have ownership/responsibility.)
-        //};
+
+        class Label : public Control {
+            std::wstring text;
+            uint32_t textBackcolor;
+            uint32_t textForecolor;
+            Align textHorizontalAlign;
+            Align textVerticalAlign;
+            bool textWrap;
+            bool textStretch;
+            backgroundFill_t backgroundFill;
+            
+        protected:
+            std::mutex mtx;
+
+            virtual void DrawControl(BufferGrid Buffer, RectU32 Partition) override;
+
+        public:
+            inline Label(FocusManager* FocusManager, std::function<void()> InvalidateFunc, std::wstring Text = L"", uint32_t TextBackcolor = 0xFF000000, uint32_t TextForecolor = 0xFFFFFFFF, Align TextHorizontalAlign = Align::Middle, Align TextVerticalAlign = Align::Middle, bool TextWrap = true, bool TextStretch = true, backgroundFill_t BackgroundFill = std::monostate{})
+                : Control(FocusManager, InvalidateFunc), text(Text), textBackcolor(TextBackcolor), textForecolor(TextForecolor), textHorizontalAlign(TextHorizontalAlign), textVerticalAlign(TextVerticalAlign), textWrap(TextWrap), textStretch(TextStretch), backgroundFill(BackgroundFill) { }
+
+            virtual std::wstring GetText();
+            virtual void SetText(const std::wstring& NewText);
+
+            virtual uint32_t GetTextBackcolor();
+            virtual void SetTextBackcolor(uint32_t NewBackcolor);
+
+            virtual uint32_t GetTextForecolor();
+            virtual void SetTextForecolor(uint32_t NewForecolor);
+
+            virtual Align GetTextHorizontalAlign();
+            virtual void SetTextHorizontalAlign(Align NewAlign);
+
+            virtual Align GetTextVerticalAlign();
+            virtual void SetTextVerticalAlign(Align NewAlign);
+
+            virtual bool GetTextWrap();
+            virtual void SetTextWrap(bool Wrap);
+
+            virtual bool GetTextStretch();
+            virtual void SetTextStretch(bool Stretch);
+
+            virtual backgroundFill_t GetBackgroundFill();
+            virtual void SetBackgroundFill(backgroundFill_t NewFill);
+        };
+
+        class Button : public Label {
+            bool isCompressed;
+            backgroundFill_t fillCompressed;
+            backgroundFill_t fillUncompressed;
+
+        public:
+            Button(FocusManager* FocusManager, std::function<void()> InvalidateFunc, std::wstring Text = L"", uint32_t TextBackcolor = 0xFF000000, uint32_t TextForecolor = 0xFFFFFFFF, Align TextHorizontalAlign = Align::Middle, Align TextVerticalAlign = Align::Middle, bool TextWrap = true, bool TextStretch = true, backgroundFill_t BackgroundFillCompressed = std::monostate{}, backgroundFill_t BackgroundFillUncompressed = std::monostate{})
+                : Label(FocusManager, InvalidateFunc, Text, TextBackcolor, TextForecolor, TextHorizontalAlign, TextVerticalAlign, TextWrap, TextStretch, BackgroundFillUncompressed), isCompressed(false), fillCompressed(BackgroundFillCompressed), fillUncompressed(BackgroundFillUncompressed) { }
+
+            bool IsCompressed();
+
+            void SetBackgroundFill(backgroundFill_t NewFill) override; // Sets both.
+
+            backgroundFill_t GetBackgroundFillCompressed();
+            void SetBackgroundFillCompressed(backgroundFill_t NewSurrounding);
+
+            backgroundFill_t GetBackgroundFillUncompressed();
+            void SetBackgroundFillUncompressed(backgroundFill_t NewSurrounding);
+        };
     }
 }
