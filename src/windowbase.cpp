5adaa91c74691760ad5992490f9c1963eb59f329
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2025-03-08 18:38:35 -0500

    Fixed char type mismatch errors

--- src/windowbase.cpp
+++ src/windowbase.cpp
@@ -37,14 +37,14 @@ inline COLORREF ConvertToColorref(uint32_t Color) {
 
 namespace btui {
     void WindowBase::UpdateFunction(bool* Initialized) {
-        WNDCLASS wc = {};
+        WNDCLASSW wc = {};
         wc.hInstance = hInstance;
         wc.lpszClassName = className.c_str();
         wc.lpfnWndProc = WindowProcStaticPlaceholder;
 
-        RegisterClass(&wc);
+        RegisterClassW(&wc);
 
-        hwnd = CreateWindowEx(
+        hwnd = CreateWindowExW(
             0,                   // Optional window styles
             className.c_str(),   // Window class
             L"",                 // Window title
@@ -350,12 +350,12 @@ namespace btui {
         case WM_DROPFILES: {
             HDROP hDrop = (HDROP)WParam;
             FileDropInfo dropInfo;
-            UINT fileCount = DragQueryFile(hDrop, 0xFFFFFFFF, nullptr, 0);
+            UINT fileCount = DragQueryFileW(hDrop, 0xFFFFFFFF, nullptr, 0);
             dropInfo.filePaths.resize(fileCount);
 
             for (UINT i = 0; i < fileCount; ++i) {
                 wchar_t filePath[MAX_PATH];
-                DragQueryFile(hDrop, i, filePath, MAX_PATH);
+                DragQueryFileW(hDrop, i, filePath, MAX_PATH);
                 dropInfo.filePaths[i] = filePath;
             }
 
